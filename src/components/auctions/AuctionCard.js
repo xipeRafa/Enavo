d47654a04b57a07fa00491970e5a019c88fe6292
rafael <rafaelsistem87@gmail.com>
2021-12-11 03:00:21 -0700

    ronnie

--- src/components/auctions/AuctionCard.js
+++ src/components/auctions/AuctionCard.js
@@ -55,21 +55,21 @@ export const AuctionCard = ({ orden }) => {
 
           <div>
           {currentUser && (
-               <div className=" pl-5 m-1 p-3 my-3 bg-white mt-5 ">
-                    <h6>Id-Orden: <span className="text-muted">{orden.id}</span>  </h6>
+               <div className=" pl-5 m-1 p-3 my-3 bg-white mt-5">
+                    <h6 className="mb-0">Id-Orden: <span className="text-muted">{orden.id}</span>  </h6>
 
-                    <p> <span className='text-muted'>comprador:</span> {orden.buyer.name} </p>
-                    <p><span className='text-muted'>correo:</span>  {orden.buyer.email} </p>
-                    <p><span className='text-muted'>telefono:</span> {orden.buyer.phone} </p>
-                    <p><span className='text-muted'>fecha:</span>  {fullDate}, {hora}</p>
-                    <p><span className='text-muted'>direccion:</span>  {orden.buyer.adress}</p>
+                    <p className="mb-0"> <span className='text-muted'>comprador:</span> {orden.buyer.name} </p>
+                    <p className="mb-0"><span className='text-muted'>correo:</span>  {orden.buyer.email} </p>
+                    <p className="mb-0"><span className='text-muted'>telefono:</span> {orden.buyer.phone} </p>
+                    <p className="mb-0"><span className='text-muted'>fecha:</span>  {fullDate}, {hora}</p>
+                    <p ><span className='text-muted'>direccion:</span>  {orden.buyer.adress}</p>
 
                     {orden.items.map((el, i) => (
                         <div key={i}>
-                    <h6>Id-producto: <span className="text-muted">{el.id}</span> </h6>
-                         <p><span className='text-muted'>producto:</span>  {el.item}</p>
-                         <p><span className='text-muted'>precio:</span>  {el.price}</p>
-                         <p><span className='text-muted'>cantidad:</span>  {el.qty}</p>
+                    <h6 className="mb-0">Id-producto: <span className="text-muted">{el.id}</span> </h6>
+                         <p className="mb-0"><span className='text-muted'>producto:</span>  {el.item}</p>
+                         <p className="mb-0"><span className='text-muted'>precio:</span>  {el.price}</p>
+                         <p className="mb-0"><span className='text-muted'>cantidad:</span>  {el.qty}</p>
                          <div>
                               <p>
                                   <span className='text-muted'>Navojoa </span>{navojoa}
